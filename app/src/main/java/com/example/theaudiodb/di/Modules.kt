63fb0beb22b289e05e89de20f7a1884b38099fca
fabianamf <fabianamf@ciandt.com>
2022-08-09 16:56:26 -0300

    creating di module and adding in the application

--- app/src/main/java/com/example/theaudiodb/di/Modules.kt
+++ app/src/main/java/com/example/theaudiodb/di/Modules.kt
@@ -1,2 +1,15 @@
 package com.example.theaudiodb.di
 
+import okhttp3.Request
+import org.koin.dsl.module
+
+val lovedDIModule = module {
+    single<Request> {
+        Request.Builder()
+            .url("https://theaudiodb.p.rapidapi.com/mostloved.php?format=track")
+            .get()
+            .addHeader("X-RapidAPI-Key", "95bec82457msh57e787fa182e343p15a28fjsn50988daa2194")
+            .addHeader("X-RapidAPI-Host", "theaudiodb.p.rapidapi.com")
+            .build()
+    }
+}
